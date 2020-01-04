2146cbf2303e4aaa564167fb08af9cf101aa64ac
Simon Bengtsson <simonbengt@gmail.com>
2020-01-04 21:00:19 +0100

    Refactor

--- web/backend/controller.dart
+++ web/backend/controller.dart
@@ -1,108 +1,123 @@
 part of snakeapp;
 
+class ArrowKey {
+  static const left = 37;
+  static const up = 38;
+  static const right = 39;
+  static const down = 40;
+
+  static const all = [left, up, right, down];
+}
+
 class KeyController {
   KeyController();
-  
-  void keyListener(Event evt){
-    KeyboardEvent kevt = evt as KeyboardEvent;
-    
-    //So that the snake can't eat itself straight after turn
-    if(kevt.keyCode >= 37 && kevt.keyCode <= 40) {
-      if(snake.segments[snake.segments.length-1].length < snake.thickness*2){
-        snake.segments[snake.segments.length-1].length = snake.thickness*2+1;
+
+  void keyListener(KeyboardEvent evt) {
+    // Prevent snake from eating itself right after turn
+    if (ArrowKey.all.contains(evt.keyCode)) {
+      var lastSegment = snake.segments[snake.segments.length - 1];
+      if (lastSegment.length < snake.thickness * 2) {
+        lastSegment.length = snake.thickness * 2 + 1;
       }
     }
-    
-    switch(kevt.keyCode) {
-      
-      //Up arrow
-      case 38:
-          //Last was left segment
-          if(snake.segments.last is RightSegment) {
-            var pos = snake.segments.last.getNewLeftPos();
-            ElemBox box = new ElemBox(querySelector('#canvas'), pos.x, pos.y,
-                Snake.DEFAULT_THICKNESS, Snake.DEFAULT_THICKNESS);
-            snake.segments.add(new UpSegment(box));
-          } 
-          //Last was right segment
-          else if (snake.segments.last is LeftSegment) {
-            var pos = snake.segments.last.getNewRightPos();
-            ElemBox box = new ElemBox(querySelector('#canvas'), pos.x, pos.y,
-                Snake.DEFAULT_THICKNESS, Snake.DEFAULT_THICKNESS);
-            snake.segments.add(new UpSegment(box));
-          } 
-          //Not allowed
-          else {
-            print('Not allowed direction');
-          }
-          break;
-          
-      //Right arrow
-      case 39:
-        //Last was left segment
-        if(snake.segments.last is UpSegment) {
-          var pos = snake.segments.last.getNewRightPos();
-          ElemBox box = new ElemBox(querySelector('#canvas'), pos.x, pos.y,
-              Snake.DEFAULT_THICKNESS, Snake.DEFAULT_THICKNESS);
-          snake.segments.add(new RightSegment(box));
-        } 
-        //Last was right segment
-        else if (snake.segments.last is DownSegment) {
-          var pos = snake.segments.last.getNewLeftPos();
-          ElemBox box = new ElemBox(querySelector('#canvas'), pos.x, pos.y,
-              Snake.DEFAULT_THICKNESS, Snake.DEFAULT_THICKNESS);
-          snake.segments.add(new RightSegment(box));
-        } 
-        //Not allowed
-        else {
-          print('Not allowed direction');
-        }
+
+    switch (evt.keyCode) {
+      case ArrowKey.up:
+        goUp();
         break;
-        
-      //Down arrow
-      case 40:
-        //Last was left segment
-        if(snake.segments.last is LeftSegment) {
-          var pos = snake.segments.last.getNewLeftPos();
-          ElemBox box = new ElemBox(querySelector('#canvas'), pos.x, pos.y,
-              Snake.DEFAULT_THICKNESS, Snake.DEFAULT_THICKNESS);
-          snake.segments.add(new DownSegment(box));
-        } 
-        //Last was right segment
-        else if (snake.segments.last is RightSegment) {
-          var pos = snake.segments.last.getNewRightPos();
-          ElemBox box = new ElemBox(querySelector('#canvas'), pos.x, pos.y,
-              Snake.DEFAULT_THICKNESS, Snake.DEFAULT_THICKNESS);
-          snake.segments.add(new DownSegment(box));
-        } 
-        //Last segment is not allowed 
-        else {
-          print('Not allowed direction');
-        }
+      case ArrowKey.right:
+        goRight();
         break;
-        
-      //Left arrow
-      case 37:
-        //Last was Up segment
-        if(snake.segments.last is UpSegment) {
-          var pos = snake.segments.last.getNewLeftPos();
-          ElemBox box = new ElemBox(querySelector('#canvas'), pos.x, pos.y,
-              Snake.DEFAULT_THICKNESS, Snake.DEFAULT_THICKNESS);
-          snake.segments.add(new LeftSegment(box));
-        } 
-        //Last was Down segment
-        else if (snake.segments.last is DownSegment) {
-          var pos = snake.segments.last.getNewRightPos();
-          ElemBox box = new ElemBox(querySelector('#canvas'), pos.x, pos.y,
-              Snake.DEFAULT_THICKNESS, Snake.DEFAULT_THICKNESS);
-          snake.segments.add(new LeftSegment(box));
-        } 
-        //Not allowed
-        else {
-          print('Not allowed direction');
-        }
+      case ArrowKey.down:
+        goDown();
+        break;
+      case ArrowKey.left:
+        goLeft();
         break;
     }
   }
-}
 
+  goDown() {
+    //Last was left segment
+    if (snake.segments.last is LeftSegment) {
+      var pos = snake.segments.last.getNewLeftPos();
+      ElemBox box = new ElemBox(querySelector('#canvas'), pos.x, pos.y,
+          Snake.DEFAULT_THICKNESS, Snake.DEFAULT_THICKNESS);
+      snake.segments.add(new DownSegment(box));
+    }
+    //Last was right segment
+    else if (snake.segments.last is RightSegment) {
+      var pos = snake.segments.last.getNewRightPos();
+      ElemBox box = new ElemBox(querySelector('#canvas'), pos.x, pos.y,
+          Snake.DEFAULT_THICKNESS, Snake.DEFAULT_THICKNESS);
+      snake.segments.add(new DownSegment(box));
+    }
+    //Last segment is not allowed
+    else {
+      print('Not allowed direction');
+    }
+  }
+
+  goUp() {
+    //Last was left segment
+    if (snake.segments.last is RightSegment) {
+      var pos = snake.segments.last.getNewLeftPos();
+      ElemBox box = new ElemBox(querySelector('#canvas'), pos.x, pos.y,
+          Snake.DEFAULT_THICKNESS, Snake.DEFAULT_THICKNESS);
+      snake.segments.add(new UpSegment(box));
+    }
+    //Last was right segment
+    else if (snake.segments.last is LeftSegment) {
+      var pos = snake.segments.last.getNewRightPos();
+      ElemBox box = new ElemBox(querySelector('#canvas'), pos.x, pos.y,
+          Snake.DEFAULT_THICKNESS, Snake.DEFAULT_THICKNESS);
+      snake.segments.add(new UpSegment(box));
+    }
+    //Not allowed
+    else {
+      print('Not allowed direction');
+    }
+  }
+
+  goRight() {
+    //Last was left segment
+    if (snake.segments.last is UpSegment) {
+      var pos = snake.segments.last.getNewRightPos();
+      ElemBox box = new ElemBox(querySelector('#canvas'), pos.x, pos.y,
+          Snake.DEFAULT_THICKNESS, Snake.DEFAULT_THICKNESS);
+      snake.segments.add(new RightSegment(box));
+    }
+    //Last was right segment
+    else if (snake.segments.last is DownSegment) {
+      var pos = snake.segments.last.getNewLeftPos();
+      ElemBox box = new ElemBox(querySelector('#canvas'), pos.x, pos.y,
+          Snake.DEFAULT_THICKNESS, Snake.DEFAULT_THICKNESS);
+      snake.segments.add(new RightSegment(box));
+    }
+    //Not allowed
+    else {
+      print('Not allowed direction');
+    }
+  }
+
+  goLeft() {
+    //Last was Up segment
+    if (snake.segments.last is UpSegment) {
+      var pos = snake.segments.last.getNewLeftPos();
+      ElemBox box = new ElemBox(querySelector('#canvas'), pos.x, pos.y,
+          Snake.DEFAULT_THICKNESS, Snake.DEFAULT_THICKNESS);
+      snake.segments.add(new LeftSegment(box));
+    }
+    //Last was Down segment
+    else if (snake.segments.last is DownSegment) {
+      var pos = snake.segments.last.getNewRightPos();
+      ElemBox box = new ElemBox(querySelector('#canvas'), pos.x, pos.y,
+          Snake.DEFAULT_THICKNESS, Snake.DEFAULT_THICKNESS);
+      snake.segments.add(new LeftSegment(box));
+    }
+    //Not allowed
+    else {
+      print('Not allowed direction');
+    }
+  }
+}
--- web/sb_snake.dart
+++ web/sb_snake.dart
@@ -35,25 +35,25 @@ void main() {
 
 void spawnApple() {
   apple = new Apple();
-  if(snake.intersects(apple.rect)) {
+  if (snake.intersects(apple.rect)) {
     apple.remove();
     spawnApple();
   }
 }
 
-void update(num delta){
+void update(num delta) {
   // Move rect
   Rectangle moveRect = snake.calcMoveRect();
-  
+
   //Move snake
-  if(isGameOver(moveRect)) {
+  if (isGameOver(moveRect)) {
     restart();
   } else {
     snake.move();
   }
- 
+
   //Check for apples
-  if (apple.rect.intersects(moveRect)){
+  if (apple.rect.intersects(moveRect)) {
     apple.remove();
     spawnApple();
     score += 100;
@@ -64,45 +64,40 @@ void update(num delta){
   window.animationFrame.then(update);
 }
 
-void restart(){
+void restart() {
   querySelector('#canvas').children.clear();
   snake = new Snake();
   apple = new Apple();
   score = 0;
 }
 
-void scoreUpdated(num score){
+void scoreUpdated(num score) {
   querySelector("#score").text = "$score";
 }
 
 /**
  * Is game over if the snake is outside of canvas or if its a cannibal
  */
-bool isGameOver(Rectangle moveRect){
+bool isGameOver(Rectangle moveRect) {
   return isOutsideCanvas(moveRect) || isCannibal(moveRect);
 }
 
-bool isOutsideCanvas(Rectangle moveRect){  
+bool isOutsideCanvas(Rectangle moveRect) {
   bool isTooLeft = moveRect.left <= 0;
   bool isTooRight = moveRect.left + moveRect.width >= canvasRect.width;
-  bool isTooTop = moveRect.top <= 0; 
+  bool isTooTop = moveRect.top <= 0;
   bool isTooBottom = moveRect.top + moveRect.height >= canvasRect.height;
-  
+
   return isTooLeft || isTooRight || isTooTop || isTooBottom;
 }
 
-bool isCannibal(Rectangle moveRect){
-  for(int i = 0; i < snake.segments.length; i++){
-    if(i != snake.segments.length - 2 && i != snake.segments.length - 1) {
-      if(snake.segments[i].intersects(moveRect)) {
+bool isCannibal(Rectangle moveRect) {
+  for (int i = 0; i < snake.segments.length; i++) {
+    if (i != snake.segments.length - 2 && i != snake.segments.length - 1) {
+      if (snake.segments[i].intersects(moveRect)) {
         return true;
       }
     }
   }
   return false;
 }
-
-
-
-
-
